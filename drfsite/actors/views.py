15b585b61cb3932c182e2ad9ad00b094b884f6e5
Farid Shamsov <farid979797@gmail.com>
2022-09-18 20:26:46 +0300

    add first APIView in views

--- drfsite/actors/views.py
+++ drfsite/actors/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from actors.models import Actors
+from actors.serializers import ActorSerializer
+
+
+class ActorAPIView(generics.ListAPIView):
+    queryset = Actors.objects.all()
+    serializer_class = ActorSerializer
